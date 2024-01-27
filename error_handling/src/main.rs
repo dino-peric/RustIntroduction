c4e2cd12005a695318f157affc758b4467cb6336
dino.peric <dino.peric2@gmail.com>
2024-01-27 14:12:16 +0100

    progress

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,81 @@
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::ErrorKind;
+
 fn main() {
     // panic!("crash and burn");
-    let v = vec![1, 2, 3];
-    v[99];
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    // Equivalent to above code using closures
+    // let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+    //     if error.kind() == ErrorKind::NotFound {
+    //         File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("Problem creating the file: {:?}", error)
+    //         })
+    //     } else {
+    //         panic!("Problem opening the file: {:?}", error)
+    //     }
+    // });
+
+    // unwrap panics on error, expect allows you to specify the panic message
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
+
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+// Equivalent to above code using ? operator
+fn read_username_from_file_() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// Even shorter
+fn read_username_from_file__() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// Even shorter
+use std::fs;
+fn read_username_from_file___() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
