f7acedb714a57dfe68c63533a4307d28eb304496
dino.peric <dino.peric2@gmail.com>
2024-03-15 11:47:33 +0100

    progress

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Continue at https://doc.rust-lang.org/book/ch10-02-traits.html
+Continue at https://doc.rust-lang.org/book/ch10-02-traits.html#using-trait-bounds-to-conditionally-implement-methods
+
+
+
+C# stuff for generics
+https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/generics
+https://learn.microsoft.com/en-us/dotnet/standard/generics/interfaces
+https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/generic-interfaces
+https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/types#84-constructed-types
+https://learn.microsoft.com/en-us/dotnet/standard/generics/covariance-and-contravariance
---
+++ traits/src/main.rs
@@ -0,0 +1,96 @@
+pub trait Summary {
+    // default implementation
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub struct WeatherForecast {
+    pub high_temp: f64,
+    pub low_temp: f64,
+    pub chance_of_precipitation: f64,
+}
+
+impl Summary for WeatherForecast {
+    // default
+}
+
+// equivalent to pub fn notify<T: Summary>(item: &T) {
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// This one can get two different types that both implement summary
+// pub fn notify(item1: &impl Summary, item2: &impl Summary)
+
+// this one has to get two items of the same type that implement summary
+// pub fn notify<T: Summary>(item1: &T, item2: &T)
+
+pub fn notify2(item: &(impl Summary + std::fmt::Display)) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// fn some_function<T, U>(t: &T, u: &U) -> i32
+// where
+//     T: Display + Clone,
+//     U: Clone + Debug,
+// {
+// }
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let weather = WeatherForecast {
+        high_temp: 75.0,
+        low_temp: 55.0,
+        chance_of_precipitation: 0.2,
+    };
+
+    println!("The weather is: {}", weather.summarize());
+}
